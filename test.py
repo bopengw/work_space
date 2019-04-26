6892b4ff1f0d36f838d5d5c8e382eb9555232fee
wbp <990879835@qq.com>
2019-04-27 00:40:57 +0800

    手写识别，RNN

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-import numpy as np
-import tensorflow as tf
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-a = tf.constant(2)
-sess = tf.Session()
-print(sess.run(a))
+# import tensorflow as tf
+# import numpy as np
+# #只用cpu
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+# b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+# c = tf.matmul(a, b)
+# # with device('\gpu1'):
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# print(sess.run(c))
+
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
